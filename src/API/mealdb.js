8938663e0b263a8a6cbc58d6b424c94f0045e875
baortega <baortega@puce.edu.ec>
2025-01-18 15:05:54 -0500

    Proyecto React

--- src/API/mealdb.js
+++ src/API/mealdb.js
@@ -1,7 +1,7 @@
 export const fetchMeals = async (query) => {
     try {
       const response = await fetch(
-        `https://www.themealdb.com/api/json/v1/1/search.php?s=${query}&lang=es` // Incluye el parámetro de idioma
+        `https://www.themealdb.com/api/json/v1/1/search.php?s=${query}&lang=es` 
       );
       const data = await response.json();
       return data.meals; // Retorna el array de recetas
